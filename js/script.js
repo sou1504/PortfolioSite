10d79c264468a0b3d5244c9fd1e811d5dd28c826
sou1504 <114889370+sou1504@users.noreply.github.com>
2023-12-20 21:57:46 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("HelloWorld");
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollLinks = document.querySelectorAll('a[href^="#"]');
+    
+    scrollLinks.forEach((scrollLink) => {
+      scrollLink.addEventListener("click", (e) => {
+        e.preventDefault();
+  
+        const hrefLink = scrollLink.getAttribute("href");
+        const targetContent = document.getElementById(hrefLink.replace("#", ""));
+  
+        if (targetContent) {
+          const rectTop = targetContent.getBoundingClientRect().top;
+          const positionY = window.pageYOffset;
+          const target = rectTop + positionY;
+  
+          window.scrollTo({
+            top: target,
+            behavior: "smooth",
+          });
+        }
+      });
+    });
+  });
